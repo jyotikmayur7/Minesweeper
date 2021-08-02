7865068cad9bd306a437cbc55d5ea99ef9fea924
Jyotik Mayur <heyjyotik@gmail.com>
2021-08-02 10:32:08 +0100

    Connected GameBoard activity with the game_board layout views

--- app/src/main/java/com/example/minesweeper/GameBoardActivity.kt
+++ app/src/main/java/com/example/minesweeper/GameBoardActivity.kt
@@ -2,10 +2,18 @@ package com.example.minesweeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class GameBoardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_board)
+
+        val minesCount: TextView = findViewById(R.id.mines)
+        val time: TextView = findViewById(R.id.time)
+        val restartGame: Button = findViewById(R.id.restart)
+
+
     }
 }
