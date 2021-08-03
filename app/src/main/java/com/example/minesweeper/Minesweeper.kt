3f4d98a8b8bd0d85928cb76a37acc7ee16d99869
Jyotik Mayur <heyjyotik@gmail.com>
2021-08-03 19:35:45 +0100

    Implemented setMines() method which will setup the mines in the board on random locations

--- app/src/main/java/com/example/minesweeper/Minesweeper.kt
+++ app/src/main/java/com/example/minesweeper/Minesweeper.kt
@@ -1,10 +1,29 @@
 package com.example.minesweeper
 
+import kotlin.random.Random
+
 class Minesweeper(private val row: Int, private val col: Int, private val mines: Int) {
     val board = Array(row){Array(col){MineCell()}}
     val status = Status.ONGOING
     private var moves = 0
     var minesCount = 0
-    private val MINES = -1
+    private val MINE = -1
 
+    fun setMines(){
+        var count = 0
+        while(count != minesCount){
+            val randomRow = Random.nextInt(0,row)
+            val randomCol = Random.nextInt(0, col)
+
+            if(board[randomRow][randomRow].value != MINE){
+                board[randomRow][randomCol].value = MINE
+                count++
+                updateNeighbours()
+            }
+        }
+    }
+
+    fun updateNeighbours(){
+
+    }
 }
