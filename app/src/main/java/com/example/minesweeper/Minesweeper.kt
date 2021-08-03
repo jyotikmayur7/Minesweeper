13e16fee4d66f610e49127cd06548f32890e3496
Jyotik Mayur <heyjyotik@gmail.com>
2021-08-03 19:22:18 +0100

    Created game board in the Minesweeper class

--- app/src/main/java/com/example/minesweeper/Minesweeper.kt
+++ app/src/main/java/com/example/minesweeper/Minesweeper.kt
@@ -1,8 +1,10 @@
 package com.example.minesweeper
 
 class Minesweeper(private val row: Int, private val col: Int, private val mines: Int) {
+    val board = Array(row){Array(col){MineCell()}}
     val status = Status.ONGOING
     private var moves = 0
     var minesCount = 0
     private val MINES = -1
+
 }
